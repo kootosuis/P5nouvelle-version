12525fd1383dd8304a11bf5c4e93d19e2cb81307
kootosuis <poursaintgervais@gmail.com>
2021-11-16 08:23:56 +0100

    deuxieme mentorat

--- front/js/cart.js
+++ front/js/cart.js
@@ -32,55 +32,59 @@ function displayLines() {
 
 //-------------------- calcul du total des quantités à la sortie de la page produit --------------------//
 
-let totalQuantity = 0;
-for (line of cart){
-     totalQuantity = (totalQuantity + line.quantity)
-}
-document.getElementById("totalQuantity").textContent  =  totalQuantity;
+// let totalQuantity = 0;
+// for (line of cart){
+//      totalQuantity = (totalQuantity + line.quantity)
+// }
+// document.getElementById("totalQuantity").textContent  =  totalQuantity;
 
 //-------------------- calcul du prix total à la sortie de la page produit --------------------//
 
-let totalPrice = 0
-for (line of cart){
-     totalPrice = totalPrice + (line.price * line.quantity)
-}
-document.getElementById("totalPrice").textContent  =  totalPrice + ",00 €";
+// let totalPrice = 0
+// for (line of cart){
+//      totalPrice = totalPrice + (line.price * line.quantity)
+// }
+// document.getElementById("totalPrice").textContent  =  totalPrice + ",00 €";
 
 
 
 //-------------------- calcul dynamique du total des quantités --------------------//
-// let totalQuantity = 0;
-// let totalPrice = 0;
-// let totalOrder = document.getElementById("cart__items");
-// let pricePerLine = 0;
+let totalQuantity = 0;
+let totalPrice = 0;
+let totalOrder = document.querySelectorAll("#cart__items");
+let pricePerLine = 0;
 
-// console.log (totalOrder);
+console.log (totalOrder);
 
-// document.getElementById(dataset.id});
+// document.getElementById(dataset.id);
 
-// console.log (article);
+for (childNode in totalOrder){
+     let quantity = parseInt(document.getElementById("quantity").value);
+     console.log("xxx" + quantity)
+     let price = document.getElementById("price").value;
+     console.log("xxx" + price)
+     pricePerLine = price * quantity;
+}
 
-// for (article in totalOrder){
-//      let quantity = parseInt(document.getElementById("quantity").value);
-//      console.log("xxx" + quantity)
-//      let price = document.getElementById("price").value;
-//      console.log("xxx" + price)
-//      pricePerLine = price * quantity;
-// }
-
-// totalQuantity = totalQuantity + quantity;
-// totalPrice = totalPrice + pricePerLine;
-// document.getElementById("totalQuantity").textContent  =  totalQuantity;
-// document.getElementById("totalPrice").textContent  =  totalPrice + ",00 €";
-
-
+totalQuantity = totalQuantity + quantity;
+totalPrice = totalPrice + pricePerLine;
+document.getElementById("totalQuantity").textContent  =  totalQuantity;
+document.getElementById("totalPrice").textContent  =  totalPrice + ",00 €";
 
 
 
 //-------------- function delete ------------------------//
 
-let elementList = document.querySelectorAll(".deleteItem");
-console.log(elementList);
+// let articleList = document.querySelectorAll(".deleteItem");
+// console.log(articleList);
+
+// for (i=0; articleList.length; i++){
+//      articleList[i].addEventListener("click", (event) => {
+//           event.preventDefault();
+//           let idArticleDelete = articleList[i].id
+//           cart = cart.filter(elt => elt.id !== idArticleDelete)
+//      })
+// }
 
 // document.getElementById("delete").onclick = (event)=> {
 //      event.preventDefault();
